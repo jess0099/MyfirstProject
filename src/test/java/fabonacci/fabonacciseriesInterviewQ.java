9a565e55758c92654674683258f45da1d372ae06
v836178 <Amit.bhati@united.com>
2023-03-04 20:38:16 +0530

    fabbonaci program

--- src/test/java/fabonacci/fabonacciseriesInterviewQ.java
+++ src/test/java/fabonacci/fabonacciseriesInterviewQ.java
@@ -2,9 +2,12 @@ package fabonacci;
 
 import java.util.Scanner;
 
+import org.testng.annotations.Test;
+
 public class fabonacciseriesInterviewQ {
 
-	public static void main(String[] args) {
+	@Test
+	public  void test1() {
 
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
@@ -12,6 +15,27 @@ public class fabonacciseriesInterviewQ {
 		for(int i=1; i<10;i++) {
 			int c=a+b;
 			System.out.println(""+c);
+			a=b;
+			b=c;
+		}
+	}
+	
+	
+	@Test
+	
+	public void testfabonacci() {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		System.out.println("value of a"+a);
+		int b=sc.nextInt();
+		System.out.println("value of b"+b);
+		for(int i=1;i<5;i++) {
+			
+			int c=a+b;
+			
+			System.out.println("fabonacci series are->"+c);
 			a=b;
 			b=c;
 		}
